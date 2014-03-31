a55fe42d5e18f5dfafd7b04c748f2534ac45370c
Eleanor Lee <ellie007@gmail.com>
2014-03-31 11:26:40 -0500

    added context blocks for game spec tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,70 +8,76 @@ describe Game do
   let(:player) { Player.new }
   let(:game)  { Game.new(board, ai, player) }
 
-  it "makes a move for a human" do
-    game.human_turn(1).should == [" X ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
-  end
-  it "makes a move for ai" do
-    game.ai_turn(2).should == ["   ", " O ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
-  end
-
-  it "has no winner at the beginning of the game" do
-    game.winner.should == nil
+  context "placement of moves on the board"
+    it "makes a move for a human" do
+      game.human_turn(1).should == [" X ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+    end
+    it "makes a move for ai" do
+      game.ai_turn(2).should == ["   ", " O ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+    end
   end
 
-  it "the ai wins the game with a diagonal" do
-    board.fill_cell(1, ai.token)
-    board.fill_cell(5, ai.token)
-    board.fill_cell(9, ai.token)
+  context "game winner determination" do
+    it "has no winner at the beginning of the game" do
+      game.winner.should == nil
+    end
 
-    game.winner.should == ai.token
-  end
-  it "the ai wins the game with a row" do
-    board.fill_cell(1, ai.token)
-    board.fill_cell(2, ai.token)
-    board.fill_cell(3, ai.token)
+    it "the ai wins the game with a diagonal" do
+      board.fill_cell(1, ai.token)
+      board.fill_cell(5, ai.token)
+      board.fill_cell(9, ai.token)
 
-    game.winner.should == ai.token
-  end
-  it "the ai wins the game with a column" do
-    board.fill_cell(1, ai.token)
-    board.fill_cell(4, ai.token)
-    board.fill_cell(7, ai.token)
+      game.winner.should == ai.token
+    end
+    it "the ai wins the game with a row" do
+      board.fill_cell(1, ai.token)
+      board.fill_cell(2, ai.token)
+      board.fill_cell(3, ai.token)
 
-    game.winner.should == ai.token
+      game.winner.should == ai.token
+    end
+    it "the ai wins the game with a column" do
+      board.fill_cell(1, ai.token)
+      board.fill_cell(4, ai.token)
+      board.fill_cell(7, ai.token)
+
+      game.winner.should == ai.token
+    end
+
+    it "the player wins the game with a diagonal" do
+      board.fill_cell(1, player.token)
+      board.fill_cell(5, player.token)
+      board.fill_cell(9, player.token)
+
+      game.winner.should == player.token
+    end
+    it "the player wins the game with a row" do
+      board.fill_cell(1, player.token)
+      board.fill_cell(2, player.token)
+      board.fill_cell(3, player.token)
+
+      game.winner.should == player.token
+    end
+    it "the player wins the game with a column" do
+      board.fill_cell(1, player.token)
+      board.fill_cell(4, player.token)
+      board.fill_cell(7, player.token)
+
+      game.winner.should == player.token
+    end
   end
 
-  it "the player wins the game with a diagonal" do
-    board.fill_cell(1, player.token)
-    board.fill_cell(5, player.token)
-    board.fill_cell(9, player.token)
-
-    game.winner.should == player.token
-  end
-  it "the player wins the game with a row" do
-    board.fill_cell(1, player.token)
-    board.fill_cell(2, player.token)
-    board.fill_cell(3, player.token)
-
-    game.winner.should == player.token
-  end
-  it "the player wins the game with a column" do
-    board.fill_cell(1, player.token)
-    board.fill_cell(4, player.token)
-    board.fill_cell(7, player.token)
-
-    game.winner.should == player.token
-  end
-
-  it "should return a false value for an invalid input type" do
-    game.valid_input?('123').should == false
-  end
-  it "should not allow the player to place in a taken cell" do
-    board.fill_cell(5, player.token)
-    game.valid_cell?(5).should == true
-  end
-  it "should allow the player to place in an empty cell" do
-    game.valid_cell?(5).should == false
+  context "player input validation" do
+    it "should return a false value for an invalid input type" do
+      game.valid_input?('123').should == false
+    end
+    it "should not allow the player to place in a taken cell" do
+      board.fill_cell(5, player.token)
+      game.valid_cell?(5).should == true
+    end
+    it "should allow the player to place in an empty cell" do
+      game.valid_cell?(5).should == false
+    end
   end
 
 end
