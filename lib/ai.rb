97ca9d9b7d0d5c37f46e4c036f85754a25002e76
Eleanor Lee <ellie007@gmail.com>
2014-06-30 12:13:28 -0500

    refactored minimax - got rid of duplication within the function

--- lib/ai.rb
+++ lib/ai.rb
@@ -11,7 +11,7 @@ class Ai
       moves = {}
       board.available_spaces(@board.cells).each do |space|
         board.cells[space] = max_token
-        moves[space] = minimax(max_token, min_token, false, board)
+        moves[space] = minimax(min_token, max_token, false, board)
         board.cells[space] = nil
       end
       moves.select { |key, value| value == moves.values.max }.keys[0]
@@ -20,30 +20,21 @@ class Ai
     end
   end
 
-  def minimax(max_token, min_token, maximizing_player, node)
+  def minimax(current_player_token, opponent_token, maximizing_player, node)
     return 0 if GameRules.is_tie?(node)
     return 1 if GameRules.winner?(node) && maximizing_player == false
     return -1 if GameRules.winner?(node) && maximizing_player == true
 
-    if maximizing_player == true
-      best_score = -100
-      node.available_spaces(node.cells).each do |space|
-        node.cells[space] = max_token
-        score = minimax(max_token, min_token, false, node)
-        node.cells[space] = nil
-        best_score = score if score > best_score
-      end
-      return best_score
-    elsif maximizing_player == false
-      best_score = 100
-      node.available_spaces(node.cells).each do |space|
-        node.cells[space] = min_token
-        score = minimax(max_token, min_token, true, node)
-        node.cells[space] = nil
-        best_score = score if score < best_score
-      end
-      return best_score
+    maximizing_player ? best_score = -100 : best_score = 100
+    operator = maximizing_player ? '>' : '<'
+
+    node.available_spaces(node.cells).each do |space|
+      node.cells[space] = current_player_token
+      score = minimax(opponent_token, current_player_token, !maximizing_player, node)
+      node.cells[space] = nil
+      best_score = score if score.send(operator, best_score)
     end
+    best_score
   end
 
 private
